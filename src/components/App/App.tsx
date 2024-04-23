3d5157377f6f2d843c33f81ceafa3f7ba502348b
Mark Nosal <mark.c.nosal@gmail.com>
2024-04-22 20:39:01 -0400

    Add event html

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Calendar from '../Calendar';
 import Recorder from '../Recorder';
 import './App.css';
 
@@ -6,6 +7,7 @@ const App: React.FC = () => {
     return (
         <div className="App">
             <Recorder />
+            <Calendar />
         </div>
     );
 }
